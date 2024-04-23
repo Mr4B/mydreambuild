69089876bb2eb6da30133b62526c2e06bba98784
Mr 4B <kevinbattistelli99@gmail.com>
2024-04-23 18:41:38 +0200

    modified:   db.sql

--- db.sql
+++ db.sql
@@ -1,15 +1,43 @@
 -- Tabelle database yourpc
 
 CREATE TABLE Utente (
-
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    nome VARCHAR(255),
+    cognome VARCHAR(255),
+    email VARCHAR(255),
+    ruolo INTEGER NOT NULL,
+    FOREIGN KEY (ruolo) REFERENCES Ruolo(id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE Ruolo (
-
+    id INTEGER PRIMARY KEY,
+    denominazione VARCHAR(255) NOT NULL
 );
 
+-- [chiamata ajax che resituisce tutti gli articoli pubblicato = true e li mette in delle card, mostrando titolo e summary]
 CREATE TABLE Articolo (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pubblicato BOOLEAN NOT NULL,
+    data_pubblicazione DATE DEFAULT NULL,
+    titolo VARCHAR(255),
+    summary VARCHAR(500),
+    testo TEXT,
+    id_redattore VARCHAR(255),
+    FOREIGN KEY (id_redattore) REFERENCES Utente(id_redattore) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
+CREATE TABLE articolo_tag (
+    id_articolo INTEGER,
+    id_tag VARCHAR(15),
+    PRIMARY KEY (id_articolo, id_zonainteresse),
+    FOREIGN KEY (id_articolo) REFERENCES Articolo(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (id_tag) REFERENCES Tag(tag) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Tag (
+    tag VARCHAR(15),
+    descrizione VARCHAR(500) DEFAULT NULL
 );
 
 CREATE TABLE Configurazione (
@@ -80,3 +108,5 @@ CREATE TABLE Periferiche (
 CREATE TABLE tipologia_periferiche (
 
 );
+
+-- Le tabelle per i tago sono da fare in futuro per una possibile ricerca di articoli
