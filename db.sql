689e4b79a6dcabfa044b178da58c8bd23a224f40
Mr 4B <kevinbattistelli99@gmail.com>
2024-04-23 17:56:36 +0200

    new file:   analisi_CAPOLAVORO.docx 	modified:   db.sql

---
+++ db.sql
@@ -0,0 +1,82 @@
+-- Tabelle database yourpc
+
+CREATE TABLE Utente (
+
+);
+
+CREATE TABLE Ruolo (
+
+);
+
+CREATE TABLE Articolo (
+
+);
+
+CREATE TABLE Configurazione (
+
+);
+
+CREATE TABLE Lista (
+
+);
+
+CREATE TABLE componenti_lista (
+
+);
+
+CREATE TABLE CPU (
+
+);
+
+CREATE TABLE Scheda_madre (
+
+);
+
+CREATE TABLE RAM (
+
+);
+
+CREATE TABLE HDD (
+
+);
+
+CREATE TABLE SSD (
+
+);
+
+CREATE TABLE ssd_type (
+
+);
+
+CREATE TABLE Alimentatore (
+
+);
+
+CREATE TABLE Case (
+
+);
+
+CREATE TABLE Raffreddamento (
+
+);
+
+CREATE TABLE coolyng_type (
+
+);
+
+CREATE TABLE Ventole (
+
+);
+
+CREATE TABLE Monitor (
+
+);
+
+CREATE TABLE Periferiche (
+
+);
+
+-- per tipologia periferiche è inteso: mouse, tastiera e telecamera
+CREATE TABLE tipologia_periferiche (
+
+);
