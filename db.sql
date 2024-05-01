e9faecccf16ba00e524fd343191c008ea815850d
Mr 4B <kevinbattistelli99@gmail.com>
2024-05-01 11:07:23 +0200

    modified:   db.sql

--- db.sql
+++ db.sql
@@ -65,6 +65,8 @@ CREATE TABLE Lista (
 non so come farla 
 una tabella per ogni componente? (lista_cpu, lista_ram, ecc...)
 un id_componente che non è foreign key e quindi poi va a cercare nelle tabelle?
+
+-- chiarire anche le immagini nel db o il percorso?
 */
 
 CREATE TABLE componenti_lista (
@@ -77,31 +79,82 @@ CREATE TABLE componenti_lista (
 -- TUTTI I COMPONENTI DEVONO AVERE LA CHIAVE PRIMARIA DELLO STESSO TIPO
 
 CREATE TABLE CPU (
-
+    id VARCHAR(9) PRIMARY KEY, -- xxxxxxxxx 1° dice la marca (intel/amd),  2/3 il modello (ix/rx), 
+    marca VARCHAR(10) NOT NULL,
+    famiglia VARCHAR(10) NOT NULL, -- (core i3, Ryzen 7, ecc...)
+    modello VARCHAR(10), -- (12400, ecc...)
+    frequenza_base DECIMAL(10,2) NOT NULL,
+    frequenza_boost DECIMAL(10,2),
+    n_core INT NOT NULL,
+    n_thread INT NOT NULL,
+    consumo_energetico INT NOT NULL,
+    dim_cache INT NOT NULL,
+    socket VARCHAR(10) NOT NULL,
+    link VARCHAR(255), --amazon
+    descrizione TEXT
 );
 
 CREATE TABLE Scheda_madre (
+    id_scheda_madre INT PRIMARY KEY AUTO_INCREMENT, --Da studiare poi
+    modello VARCHAR(255) NOT NULL,
+    formato VARCHAR(255) NOT NULL,
+    socket VARCHAR(255) NOT NULL,
+    chipset VARCHAR(255) NOT NULL,
+    numero_slot_ram INT NOT NULL,
+    tipologia_ram VARCHAR(255) NOT NULL,
+    numero_slot_pcie INT NOT NULL,
+    version_pcie VARCHAR(255) NOT NULL,
+    link VARCHAR(255), --amazon
+    descrizione TEXT
 
 );
 
 CREATE TABLE RAM (
-
+    ram_id INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(255),
+    modello VARCHAR(255),
+    dimensione INT,
+    velocita INT, --MHz
+    tipo VARCHAR(50), --ddrx
+    link VARCHAR(255), --amazon
+    descrizione TEXT
 );
 
 CREATE TABLE HDD (
-
+    id_hdd INT PRIMARY KEY AUTO_INCREMENT,
+    marca VARCHAR(255),
+    modello VARCHAR(255) NOT NULL,
+    capacita_gb INT NOT NULL,
+    fattore_di_forma VARCHAR(255) NOT NULL, --3,5 pollici
+    velocita_rotazione INT NOT NULL,
+    tipologia_interfaccia VARCHAR(255) NOT NULL, --sata 6 0 gb
+    cache_mb INT NOT NULL
+    link VARCHAR(255), --amazon
+    descrizione TEXT
 );
 
 CREATE TABLE SSD (
-
+    id_ssd INT PRIMARY KEY AUTO_INCREMENT,
+    modello VARCHAR(255) NOT NULL,
+    capacita_tb INT NOT NULL,
+    tipologia_ssd VARCHAR(255) NOT NULL,
+    fattore_di_forma VARCHAR(255) NOT NULL, --m.2
+    interfaccia VARCHAR(255) NOT NULL, --NVMe PCIe
+    velocita_lettura_mb_s INT NOT NULL,
+    velocita_scrittura_mb_s INT NOT NULL
+    link VARCHAR(255), --amazon
+    descrizione TEXT
 );
 
-CREATE TABLE ssd_type (
-
-);
-
-CREATE TABLE Alimentatore (
-
+CREATE TABLE PSU (
+    id_psu INT PRIMARY KEY AUTO_INCREMENT,
+    marca VARCHAR(255) NOT NULL,
+    modello VARCHAR(255) NOT NULL,
+    fattore_di_forma VARCHAR(255) NOT NULL, --ATX
+    watt INT,
+    schema_alimentazione VARCHAR(255) NOT NULL, --modulare, semi-modulare, ...
+    link VARCHAR(255), --amazon
+    descrizione TEXT
 );
 
 CREATE TABLE Case (
