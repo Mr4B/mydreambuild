d8c5e6361a4079bbbfdc1f0235cece9e8847e6e8
Mr 4B <kevinbattistelli99@gmail.com>
2024-05-06 15:08:15 +0200

    new file:   app/webservices/common/auth.php 	modified:   app/webservices/ws_prodotti.php 	modified:   vendor/firebase/php-jwt (modified content)

---
+++ app/webservices/common/auth.php
@@ -0,0 +1,69 @@
+<?php
+require __DIR__ . '../../../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+// Funzione per verificare l'autenticazione dell'utente utilizzando il token JWT
+function authenticateUser() {
+    global $headers;
+
+    // Controlla se l'header Authorization è presente nella richiesta
+    if (!isset($headers['Authorization'])) {
+        // Se l'header Authorization non è presente, l'utente non è autenticato
+        header("HTTP/1.1 401 Unauthorized");
+        http_response_code(401); //Unauthorized
+        exit();
+    }
+
+    // Estrai il token JWT dall'header Authorization
+    $authHeader = $headers['Authorization'];
+
+    // Verifica che l'header Authorization contenga un token JWT valido
+    if (preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
+        $token = $matches[1];
+
+        // Decodifica il token JWT utilizzando la chiave segreta
+        $key = 'ciao';
+        try {
+            $decodedToken = JWT::decode($token, new Key($key, 'HS256'));
+
+            // L'utente è autenticato
+            return $token;
+        } catch (Exception $e) {
+            // Errore nella decodifica del token JWT (token non valido o scaduto)
+            header("HTTP/1.1 401 Unauthorized");
+            exit();
+        }
+    } else {
+        // Formato dell'header Authorization non corretto
+        header("HTTP/1.1 401 Unauthorized");
+        exit();
+    }
+}
+
+// Funzione per ottenere il ruolo dall'utente dal token JWT
+function getUserRole($token) {
+    try {
+        // Decodifica il token JWT utilizzando la chiave segreta
+        $key = 'ciao';
+        $decodedToken = JWT::decode($token, new Key($key, 'HS256'));
+
+        // Verifica che il token decodificato contenga il campo 'ruolo'
+        if (isset($decodedToken->ruolo)) {
+            // Restituisci il ruolo dell'utente
+            return $decodedToken->ruolo;
+        } else {
+            // Il token non contiene il campo 'ruolo'
+            throw new Exception("Il token JWT non contiene il campo 'ruolo'.");
+        }
+    } catch (Exception $e) {
+        // Gestione degli errori durante la decodifica del token JWT
+        // Puoi registrare l'errore, inviare una risposta di errore, ecc.
+        return null; // o gestisci l'errore in un altro modo
+    }
+}
+
+
+
+?>
--- app/webservices/ws_prodotti.php
+++ app/webservices/ws_prodotti.php
@@ -1,8 +1,6 @@
 <?php
 include '../../db_connect.php';
-require __DIR__ . '/../../vendor/autoload.php';
-
-use \Firebase\JWT\JWT;
+include 'common/auth.php';
 
 //Recupera il metodo richiesto
 $method = $_SERVER['REQUEST_METHOD'];
@@ -13,69 +11,6 @@ $headers = getallheaders();
 $content_type = 'application/json';
 // Risposta da restituire
 $content_type_response = 'application/json';
-
-
-// Funzione per verificare l'autenticazione dell'utente utilizzando il token JWT
-// Funzione per autenticare un utente dato il token JWT e restituire i dati dell'utente se autenticato
-function authenticateUser() {
-    global $headers;
-
-    // Controlla se l'header Authorization è presente nella richiesta
-    if (!isset($headers['Authorization'])) {
-        // Se l'header Authorization non è presente, l'utente non è autenticato
-        header("HTTP/1.1 401 Unauthorized");
-        exit();
-    }
-
-    // Estrai il token JWT dall'header Authorization
-    $authHeader = $headers['Authorization'];
-
-    // Verifica che l'header Authorization contenga un token JWT valido
-    if (preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
-        $token = $matches[1];
-
-        // Decodifica il token JWT utilizzando la chiave segreta
-        try {
-            $decodedToken = \Firebase\JWT\JWT::decode($token, 'il_tuo_segreto', array('HS256'));
-
-            // Ora puoi accedere ai dati dell'utente dal token decodificato, se necessario
-            // Ad esempio, $decodedToken->userId, $decodedToken->username, etc.
-
-            // Restituisci i dati dell'utente
-            return $decodedToken;
-        } catch (Exception $e) {
-            // Errore nella decodifica del token JWT (token non valido o scaduto)
-            header("HTTP/1.1 401 Unauthorized");
-            exit();
-        }
-    } else {
-        // Formato dell'header Authorization non corretto
-        header("HTTP/1.1 401 Unauthorized");
-        exit();
-    }
-}
-
-
-// Funzione per verificare il ruolo dell'utente
-/* function checkRole($requiredRole) {
-
-    global $headers;
-
-    // Ottieni il token JWT dall'header della richiesta
-    $jwt = $headers['Authorization'];
-
-    // Decodifica il token JWT per ottenere i dettagli dell'utente, compreso il ruolo
-    $decodedToken = decodeJWT($jwt);
-
-    // Ottieni il ruolo dell'utente dal token decodificato
-    $userRole = $decodedToken['role'];
-
-    // Verifica se il ruolo dell'utente corrisponde al ruolo richiesto
-    if ($userRole != $requiredRole) {
-        header("HTTP/1.1 403 Forbidden");
-        exit();
-    }
-} */
 
 authenticateUser();
 
@@ -88,29 +23,96 @@ switch ($method) {
 
         // Recupera il valore del parametro "action" dall'URL
         $action = isset($_GET['action']) ? $_GET['action'] : '';
-        $search = isset($_GET['search']) ? $_GET['id'] : '';
+        $id = isset($_GET['id']) ? $_GET['id'] : '';
+
+        $cat = null;
 
         switch($action) {
-            case 'get_':
-                // Azione per recuperare i primi dati
-                $query = "";
+            case 'get_products':
+                // Ritorna tutti i prodotti
+                $query = "SELECT * FROM Prodotto;";
+                break;
+                
+            case 'get_byID':
+                $query = "SELECT * FROM Prodotto WHERE id_prodotto = ?;";
                 break;
 
-            case 'get_':
-                $query = "";
+            case 'get_GPU':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 1;
                 break;
 
-            case 'get_':
-                $query = "";
+            case 'get_CPU':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 2;
+                break;
+
+            case 'get_motherboard':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 3;
+                break;
+            
+            case 'get_RAM':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 4;
+                break;
+
+            case 'get_SSD':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 5;
+                break;
+
+            case 'get_HDD':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 6;
+                break;
+
+            case 'get_PSU':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 7;
+                break;
+
+            case 'get_case':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 8;
+                break;
+
+            case 'get_cooling':
+                // Ritorna tutte le schede video
+                $query = "SELECT * FROM Prodotto WHERE id_categoria = ?;";
+                $cat = 9;
                 break;
 
             default:
-                $query = "SELECT * FROM Veicolo LIMIT 1;";
+                echo json_encode(['errore' => 'Indirizzo errato']);
+                http_response_code(404);
                 break;
         }
 
-        /* Recupero dei dati dalla tabella */
-        $result = $conn->query($query);
+        $stmt = $conn->prepare($query);
+
+        if(isset($cat)){
+            $stmt->bind_param("i", $cat);
+        }
+
+        if($id != '') {
+            $stmt->bind_param("i", $id);
+        }
+        
+        // Esecuzione query
+        $stmt->execute();
+
+        // Recupero dei risultati
+        $result = $stmt->get_result();
+
         $lista = Array();
 
         while ($row = $result->fetch_assoc()) {
@@ -126,49 +128,58 @@ switch ($method) {
 
     case 'POST':
         // Recupero del payload (body of message)
+        $userRole = getUserRole(authenticateUser());
+        if($userRole === 'admin' || $userRole === 'moderator') {
+            // Solo un autente autorizzato può accedere a questo codice
 
-        $payload = file_get_contents('php://input');
+            $payload = file_get_contents('php://input');
 
 
-        // Trasformazione del payload nell'array che rappresenta il prodotto
-        if ($content_type == 'application/json') {
-            $data =  json_decode($payload,true);
+            // Trasformazione del payload nell'array che rappresenta il prodotto
+            if ($content_type == 'application/json') {
+                $data =  json_decode($payload,true);
+            } else {
+                echo json_encode(['errore' => 'Content-Type non ammesso']);
+                http_response_code(400); //BAD REQUEST
+                exit();
+            }
+
+            header("Content-Type: application/json; charset=UTF-8");
+
+            // Recupera il valore del parametro "action" dall'URL
+            $action = isset($_GET['action']) ? $_GET['action'] : '';
+
+            switch($action) {
+                case 'post_':
+
+                    $query = "";
+                    break;
+
+                case 'post_':
+                    $query = "";
+                    break;
+
+                default:
+                    $query = "SELECT * FROM Veicolo LIMIT 1;";
+                    break;
+            }
+
+
+            $result = $conn->query($query);
+
+            if ($result){
+                echo json_encode($user);
+                http_response_code(200);
+            }else {
+                echo json_encode(['errore' => $conn->error]);
+                http_response_code(400); //BAD REQUEST
+            }
         } else {
-            echo json_encode(['errore' => 'Content-Type non ammesso']);
-            http_response_code(400); //BAD REQUEST
-            exit();
-        }
-
-        header("Content-Type: application/json; charset=UTF-8");
-
-        // Recupera il valore del parametro "action" dall'URL
-        $action = isset($_GET['action']) ? $_GET['action'] : '';
-
-        switch($action) {
-            case 'post_':
-
-                $query = "";
-                break;
-
-            case 'post_':
-                $query = "";
-                break;
-
-            default:
-                $query = "SELECT * FROM Veicolo LIMIT 1;";
-                break;
+            echo json_encode(['errore' => 'Unauthorized']);
+            http_response_code(401); //BAD REQUEST
         }
 
 
-        $result = $conn->query($query);
-
-        if ($result){
-            echo json_encode($user);
-            http_response_code(200);
-        }else {
-            echo json_encode(['errore' => $conn->error]);
-            http_response_code(400); //BAD REQUEST
-        }
         break;
 
     case 'PUT':
