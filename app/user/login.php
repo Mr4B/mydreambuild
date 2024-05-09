b7a3f06daf53c30ee6735dac343950db9c2860a1
Mr 4B <kevinbattistelli99@gmail.com>
2024-05-09 17:12:18 +0200

    modified:   app/user/login.php

--- app/user/login.php
+++ app/user/login.php
@@ -3,9 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $("#loginForm").submit(function(){
+            // Dati da inviare al server
+            var formData = {
+                username: $("#username").val(),
+                password: $("#password").val()
+            };
+            $.ajax({
+                url: 'http://localhost/capolavoro/app/webservices/ws_accesso.php?action=login',
+                type: 'POST',
+                dataType: 'json',
+                headers: {
+                    'Accept': 'application/json'
+                },
+                contentType: "application/json",
+                data: JSON.stringify(formData),
+                success: function(data) {
+                    console.log(response);
+                },
+                error: function(xhr, status, error) {
+                    console.error('Errore durante la richiesta:', status, error);
+                }
+            });            
+            return false;
+        });
+    });
+    </script>
+
 </head>
 <body>
-    
+    <h2>Login</h2>
+    <form id="loginForm">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Login">
+    </form>
 </body>
 </html>
